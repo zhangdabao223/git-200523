7cf97775d788c65662279218016a511b176796a6
dabao <dabao@atguigu.com>
2020-10-12 20:05:36 +0800

    hot-fix commit

--- src/main/java/com/atguigu/app/HelloWorld.java
+++ src/main/java/com/atguigu/app/HelloWorld.java
@@ -3,5 +3,6 @@ package com.atguigu.app;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GIt!!");
+        System.out.println("hot-fix");
     }
 }
