f05a3eba04fe1aca806a3c3bda5ef00d02b2615f
dabao <dabao@atguigu.com>
2020-10-12 20:10:38 +0800

    second commit

--- src/main/java/com/atguigu/app/HelloWorld.java
+++ src/main/java/com/atguigu/app/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GIt!!");
         System.out.println("hot-fix");
+        System.out.println("Hello world!!");
     }
 }
